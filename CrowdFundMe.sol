41b6107853254805f2ee3e76c939d9b97d7a7900
Ghostsmaw <ghostsmaw@gmail.com>
2025-05-25 21:55:18 +0100

    Worked on the withdraw function

--- CrowdFundMe.sol
+++ CrowdFundMe.sol
@@ -2,9 +2,11 @@
 
 pragma solidity 0.8.24;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract CrowdFundMe {
+
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 5;
 
@@ -12,25 +14,21 @@ contract CrowdFundMe {
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
-        require(msg.value >=minimumUsd, "Not enough amount");
+        require(msg.value.getConversionRate() >=minimumUsd, "Not enough amount");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
     
-    function fund(uint256 value) public payable {
-    }
+    function fund(uint256 value) public payable {}
 
-    function withdraw() public {}
-
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
-    }
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess,"Call Failed");
     }
 
 }
